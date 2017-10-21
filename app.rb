b69a7682d535ea2106758b9faa5e67171f135caf
wtnabe <wtnabe@gmail.com>
2017-10-21 15:06:59 +0900

    fluent-logger を使っているつもりで標準の logger を使っていた問題を修正

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'fluent-logger'
 
-configure do
-  logger = Fluent::Logger::FluentLogger.new('fluentd-lesson', host: '127.0.0.1', port: 24224)
+before do
+  @fluent = Fluent::Logger::FluentLogger.new('fluentd-lesson', host: '127.0.0.1', port: 24224)
 end
 
 get '/' do
-  logger.info 'logged from fluent-logger'
+  @fluent.post('info', {message: 'logged from fluent-logger'})
   'Hello, World'
 end
